8998f8a0d9a3c371fa518269fc8f1019fa310400
Mohil9913 <mokariamohil@gmail.com>
2025-07-07 19:50:23 +0530

    dependency with component

---
+++ src/main/java/com/example/dependency_injection/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package com.example.dependency_injection.controllers;
+
+import com.example.dependency_injection.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/com/example/dependency_injection/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.dependency_injection.controllers;
+
+import com.example.dependency_injection.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void sayHello() {
+        System.out.println(controller.sayHello());
+    }
+}
